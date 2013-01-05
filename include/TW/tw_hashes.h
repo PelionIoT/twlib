a3f5cfbe16c31f288bcc23d0891e875cf6de3307
edhemphill <ed@wigwag.com>
2013-01-05 17:53:16 -0600

    Import from old source management

---
+++ include/TW/tw_hashes.h
@@ -0,0 +1,32 @@
+/*
+ * tw_hashes.h
+ *
+ *  Created on: Nov 25, 2011
+ *      Author: ed
+ * (c) 2011, Framez LLC
+ */
+
+#ifndef TW_HASHES_H_
+#define TW_HASHES_H_
+
+#include <limits.h>
+
+namespace TWlib {
+
+template<class T>
+struct tw_hash {
+	size_t operator()(T x) const {
+		return 0;
+	}
+};
+
+struct hashInt {
+	inline size_t operator()(const int &x) const {
+		return (size_t) (x);
+	}
+};
+
+
+
+} // end namespace
+#endif /* TW_HASHES_H_ */
---
+++ tw_socktask.cpp
@@ -0,0 +1,5 @@
+// Framez LLC
+// (c) 2010
+// tw_socktask.cpp
+// Author: ed
+
