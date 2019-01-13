9344c25e48be800e871e25c475c11a749aa26eb5
edhemphill <edhemphill@gmail.com>
2019-01-12 23:20:03 -0600

    install-deps.sh

---
+++ deps/install-deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SELF="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+DEPS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DEPS_DIR
+
+PREFIX=$DEPS_DIR
+
+cd sparsehash
+./configure --host=x86 --prefix=$PREFIX
+make
+make install
+
